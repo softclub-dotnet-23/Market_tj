c34d994deebb49458ea3fa61f2a3a5bdf24ef24e
Nekruz Rashidov <nekruzrashidov@gmail.com>
2026-07-17 11:15:42 +0500

    add dtos for Favorite

---
+++ backend/MarketTJ.Application/Dto/FavoriteDto/DeleteFavoriteDto.cs
@@ -0,0 +1,6 @@
+namespace MarketTJ.Application.Dto.FavoriteDto;
+
+public class DeleteFavoriteDto
+{
+    public int Id { get; set; }
+}
---
+++ backend/MarketTJ.Application/Dto/FavoriteDto/UpdateFavoriteDto.cs
@@ -0,0 +1,8 @@
+namespace MarketTJ.Application.Dto.FavoriteDto;
+
+public class UpdateFavoriteDto
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public int ProductListingId { get; set; }
+}
