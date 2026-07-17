c441e6f81d7f5a3884e107591db1c589c1317932
Nekruz Rashidov <nekruzrashidov@gmail.com>
2026-07-17 19:30:11 +0500

    add service for ProductImage

--- backend/MarketTJ.Application/DependencyInjection.cs
+++ backend/MarketTJ.Application/DependencyInjection.cs
@@ -20,6 +20,7 @@ public static class DependencyInjection
         services.AddScoped<ICategoryService, CategoryService>();
         services.AddScoped<IProductService, ProductService>();
         services.AddScoped<IProductListingService, ProductListingService>();
+        services.AddScoped<IProductImageService, ProductImageService>();
 
         return services;
     }
