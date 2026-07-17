158d3f3610da345791f06e0815f2704d2c286d83
Nekruz Rashidov <nekruzrashidov@gmail.com>
2026-07-17 10:45:32 +0500

    add dtos for CustomerProfile

---
+++ backend/MarketTJ.Application/Dto/CustomerProfileDto/UpdateCustomerProfileDto.cs
@@ -0,0 +1,13 @@
+using MarketTJ.Domain.Enums;
+
+namespace MarketTJ.Application.Dto.CustomerProfileDto;
+
+public class UpdateCustomerProfileDto
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public CustomerType CustomerType { get; set; }
+    public string? DefaultAddress { get; set; }
+    public string Region { get; set; } = null!;
+    public string District { get; set; } = null!;
+}
