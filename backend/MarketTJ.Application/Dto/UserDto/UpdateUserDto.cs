f9fbeee6dbe8d250f591834af9a6b54408b2bc88
Nekruz Rashidov <nekruzrashidov@gmail.com>
2026-07-17 10:38:50 +0500

    add dtos for User

---
+++ backend/MarketTJ.Application/Dto/UserDto/UpdateUserDto.cs
@@ -0,0 +1,14 @@
+using MarketTJ.Domain.Enums;
+
+namespace MarketTJ.Application.Dto.UserDto;
+
+public class UpdateUserDto
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+    public UserRole Role { get; set; }
+    public bool IsActive { get; set; }
+}
