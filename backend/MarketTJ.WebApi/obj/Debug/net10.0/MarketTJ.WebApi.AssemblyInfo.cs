b0941533c94106e58ff873d545c957eb03f276e4
shahnozasultonalizoda <shahnozasultonalizoda@gmail.com>
2026-07-20 18:30:28 +0500

    Update frontend

--- backend/MarketTJ.WebApi/obj/Debug/net10.0/MarketTJ.WebApi.AssemblyInfo.cs
+++ backend/MarketTJ.WebApi/obj/Debug/net10.0/MarketTJ.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketTJ.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d578e4676577ce68f205c74736b93c86a7aaa4a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef2dc98d08e238cb5f44b6dd25488d6feda85237")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketTJ.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketTJ.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
